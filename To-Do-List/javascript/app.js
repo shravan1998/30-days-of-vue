51e6735367b8b99aedf969067f8463ff4dd26ae2
shravan1998 <shrkum51@gmail.com>
2018-09-09 13:40:38 +0530

    Work in Progress

--- To-Do-List/javascript/app.js
+++ To-Do-List/javascript/app.js
@@ -1,10 +1,14 @@
 new Vue({
     el:'#vue-app',
-    
+    data:{
+        task:'',
+        
+    },
    
     methods:{
-        showtask:function(input){
-            return this.$refs.input.focus();
+        showtask:function(){
+            console.log(this.$refs.input.value);
+            this.task=this.$refs.input.value;
         }
     }
 
